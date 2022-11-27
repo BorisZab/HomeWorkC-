5cf99761a647d8af8e3217a9ffdaa269df683212
BorisZ <boriszab1@yandex.ru>
2022-11-27 18:53:35 +0300

    add task001

---
+++ Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+Console.Write("Первое число больше чем второе.");
+else
+Console.Write("Второе число больше чем второе.");
