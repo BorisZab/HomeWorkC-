4681350720ce271bf1698c619b3d92212244e29a
BorisZ <boriszab1@yandex.ru>
2022-11-27 20:05:23 +0300

    add task004

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n %2 == 0)
-Console.Write("Число четное");
-else 
-Console.Write("Число не четное");
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.Write(i);
