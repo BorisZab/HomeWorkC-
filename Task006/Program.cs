3074e6c7f48e9e88732f9f1ad08aa99c028bc88e
BorisZ <boriszab1@yandex.ru>
2022-12-01 18:36:43 +0300

    add task006

---
+++ Task006/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+Console.WriteLine("третьей цифры нет");
+while ( n > 999)
+    { n= n / 10;
+    }
+Console.WriteLine($"Третье число {n%10}");
