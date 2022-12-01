7a2ef0735356756cfa737106d3fe667ca7cec185
BorisZ <boriszab1@yandex.ru>
2022-12-01 18:47:36 +0300

    add task007

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,4 +1,9 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 999)
+{
+    Console.Write("Вы ошиблись!\nВведите трех значное число:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
 Console.WriteLine(n / 10 % 10);
---
+++ Task007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+Console.WriteLine("yes");
+else
+Console.WriteLine("no");
