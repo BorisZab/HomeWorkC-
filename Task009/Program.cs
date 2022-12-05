dd6d3072d0cb540b20af0a7ebfa1dd740cf86c2c
BorisZ <boriszab1@yandex.ru>
2022-12-06 00:38:49 +0300

    fix bug

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -6,9 +6,8 @@ while (n < 10000 || n > 99999)
     Console.Write("Вы ошиблись!\nВведите пятизначное число:");
     n = Convert.ToInt32(Console.ReadLine());
 }
-if (n / 10000 == n % 10 || n / 1000 == n % 100)
-//     if (n / 1000 == n % 100)
-Console.Write("Число палиндром");
-// }
-else
-Console.Write("Число не палиндром");
+if (n / 10000 == n % 10 && n / 1000 % 10 == n % 100 / 10)
+    Console.Write("Число палиндром");
+    else
+    Console.Write("Число не палиндром");
+    // Console.WriteLine(n % 100);
