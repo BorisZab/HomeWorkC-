4f4d6c0fdf1f6877a8f8305cee8c7585a9aecaf7
BorisZ <boriszab1@yandex.ru>
2022-12-05 20:33:49 +0300

    add task 009

---
+++ Task009/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Вы ошиблись!\nВведите пятизначное число:");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n / 10000 == n % 10 || n / 1000 == n % 100)
+//     if (n / 1000 == n % 100)
+Console.Write("Число палиндром");
+// }
+else
+Console.Write("Число не палиндром");
