123e21ddf1ea057a35e8f75608e42c85a0c61682
BorisZ <boriszab1@yandex.ru>
2022-12-09 18:40:50 +0300

    fix bugs

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -2,8 +2,10 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n < 100)
-Console.WriteLine("третьей цифры нет");
-while ( n > 999)
-    { n= n / 10;
-    }
-Console.WriteLine($"Третье число {n%10}");
+    Console.WriteLine("третьей цифры нет");
+ while (n > 999)
+ {
+    n = n / 10;
+ }
+ if (n > 100)
+ Console.WriteLine($"Третье число {n % 10}");
