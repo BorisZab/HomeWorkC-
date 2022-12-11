8b258381ac0dd87d728d85007acf51f96c2bbd61
BorisZ <boriszab1@yandex.ru>
2022-12-11 19:44:59 +0300

    add task 014

---
+++ Task014/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент { i +1} массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]"); 
