9988183c3e6a7f207051046ff3975f863979babf
BorisZ <boriszab1@yandex.ru>
2022-12-11 19:41:25 +0300

    add task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a > 1)
+{
+    sum = a % 10 + sum;
+    a = a / 10;
+}
+Console.WriteLine(sum);
