e13ecc3ae1944e8037f20a150083fe3582e2ee90
BorisZ <boriszab1@yandex.ru>
2022-12-11 19:28:03 +0300

    add task 012

---
+++ Task012/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int result = 1;
+while (i <= b)
+{
+    result = result * a;
+    i++;
+}
+Console.WriteLine(result);
